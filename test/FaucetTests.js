755f864593467c60e61def7f00aadcaa8ec44959
Payam Beigi <beigi.payam@gmail.com>
2022-12-29 18:01:40 -0800

    Create Chai tests to: check the owner is performing tasks

---
+++ test/FaucetTests.js
@@ -0,0 +1,35 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+
+describe('Faucet', function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployContractAndSetVariables() {
+    const initialBalance = ethers.utils.parseUnits("1", "ether");
+
+    const Faucet = await ethers.getContractFactory('Faucet');
+    const faucet = await Faucet.deploy({value: initialBalance});
+
+    const [owner, intruder] = await ethers.getSigners();
+    let withdrawAmount = ethers.utils.parseUnits("1", "ether");
+
+    console.log('Signer 1 address: ', owner.address);
+    return { faucet, owner, intruder, withdrawAmount };
+  }
+
+  it('should deploy and set the owner correctly', async function () {
+    const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
+    expect(await faucet.owner()).to.equal(owner.address);
+  });
+
+  it('should not allow a withdraw above 1 ETH at a time!', async function () {
+    const { faucet, withdrawAmount } = await loadFixture(deployContractAndSetVariables);
+    await expect(faucet.withdraw(withdrawAmount)).to.be.reverted;
+  });
+
+  it('should destory the faucet contrarct only by the owner', async function () {
+    const { faucet, intruder } = await loadFixture(deployContractAndSetVariables);
+    await expect(faucet.connect(intruder).destroyFaucet()).to.be.reverted;
+  });
+});
