8b425f5b0c62769711b31f96bbcb141469214754
Payam Beigi <beigi.payam@gmail.com>
2022-12-29 18:45:00 -0800

    Dry the code, add comments

--- test/FaucetTests.js
+++ test/FaucetTests.js
@@ -7,16 +7,22 @@ describe('Faucet', function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployContractAndSetVariables() {
+    //connecting to Alchemy network testnet @ goerli
+    const provider = ethers.getDefaultProvider(network=5, {
+      alchemy: process.env.ALCHEMY_API_KEY,
+  });
+    //initial value to deploy for the contract instance
     const initialBalance = ethers.utils.parseUnits("1", "ether");
 
     const Faucet = await ethers.getContractFactory('Faucet');
     const faucet = await Faucet.deploy({value: initialBalance});
 
-    const [owner, intruder] = await ethers.getSigners();
+    //User is someone who does not own the contract
+    const [owner, user] = await ethers.getSigners();
     let withdrawAmount = ethers.utils.parseUnits("1", "ether");
 
     console.log('Signer 1 address: ', owner.address);
-    return { faucet, owner, intruder, withdrawAmount };
+    return { faucet, owner, user, withdrawAmount, provider };
   }
 
   it('should deploy and set the owner correctly', async function () {
@@ -30,15 +36,12 @@ describe('Faucet', function () {
   });
 
   it('should destory the faucet contrarct only by the owner', async function () {
-    const { faucet, intruder } = await loadFixture(deployContractAndSetVariables);
-    await expect(faucet.connect(intruder).destroyFaucet()).to.be.reverted;
+    const { faucet, user } = await loadFixture(deployContractAndSetVariables);
+    await expect(faucet.connect(user).destroyFaucet()).to.be.reverted;
   });
 
   it('should destory the faucet contrarct when desctruct called by the owner', async function () {
-    const { faucet } = await loadFixture(deployContractAndSetVariables);
-    const provider = ethers.getDefaultProvider(network=5, {
-      alchemy: process.env.ALCHEMY_API_KEY,
-  });
+    const { faucet, provider } = await loadFixture(deployContractAndSetVariables);
     expect(await provider.getCode(faucet.address)).to.be.equal("0x");
   });
 
